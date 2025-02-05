825940d24cf4787437655525023b149609c03fd7
Colby0506 <Colby0506@hotmail.com>
2025-02-05 17:37:29 -0500

    Basic Setup

---
+++ recipe-suggester-frontend/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FC9F5B',
+        secondary: '#FBD1A2',
+        tertiary: '#ECE4B7',
+        accent: '#7DCFB6',
+        highlight: '#33CA7F',
+      },
+    },
+  },
+  plugins: [],
+}
+
