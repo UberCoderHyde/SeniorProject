1dc89debc1c5d6a13a31dfa27dfa4132243fd1e6
Colby0506 <Colby0506@hotmail.com>
2025-02-19 18:54:11 -0500

    installed fixes

--- recipe-suggester-backend/backend.bat
+++ recipe-suggester-backend/backend.bat
@@ -6,7 +6,11 @@ if not exist venv (
     echo Creating virtual environment...
     python -m venv venv
 )
-
+venv\Scripts\python -m ensurepip --default-pip
+venv\Scripts\python -m pip install --upgrade pip setuptools wheel
+venv\Scripts\python -m pip install django
+venv\Scripts\python -m pip install djangorestframework
+venv\Scripts\python -m pip install django-cors-headers
 :: Step 6: Start Django server
 echo run venv\Scripts\activate in your terminal to do anything else with the terminal
 echo Starting Django Backend...
