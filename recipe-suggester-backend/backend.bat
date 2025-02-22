9fc6a36b52a2add07652a865092cb8e87578922a
Nikhita Anusuri <nanusuri@charlotte.edu>
2025-02-22 08:24:58 -0500

    install requirements.txt in backend.bat

--- recipe-suggester-backend/backend.bat
+++ recipe-suggester-backend/backend.bat
@@ -11,6 +11,7 @@ venv\Scripts\python -m pip install --upgrade pip setuptools wheel
 venv\Scripts\python -m pip install django
 venv\Scripts\python -m pip install djangorestframework
 venv\Scripts\python -m pip install django-cors-headers
+venv\Scripts\python -m pip install -r requirements.txt
 :: Step 6: Start Django server
 echo run venv\Scripts\activate in your terminal to do anything else with the terminal
 echo Starting Django Backend...
