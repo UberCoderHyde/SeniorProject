c8938fc1537a25288129b1b3c1a3474d44ef1caf
Colby0506 <Colby0506@hotmail.com>
2025-03-23 21:05:10 -0400

    database change

--- recipe-suggester-backend/backend/settings.py
+++ recipe-suggester-backend/backend/settings.py
@@ -57,9 +57,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rsdb',       
+        'USER': 'admin',           
+        'PASSWORD': 'password',
+        'HOST': '97.89.112.225',
+        'PORT': '5432',        }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
