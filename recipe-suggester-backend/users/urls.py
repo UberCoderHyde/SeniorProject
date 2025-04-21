a798e2386b8dba592f62b4ad0376a4705b5e4c4e
Nikhita <nanusuri@charlotte.edu>
2025-04-21 08:31:11 -0400

    add a url for unsubscribe api

--- recipe-suggester-backend/users/urls.py
+++ recipe-suggester-backend/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginUserView, RegisterUserView, user_profile
+from .views import LoginUserView, RegisterUserView, user_profile, unsubscribe
 
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('profile/', user_profile, name='user-profile'),
+    path('unsubscribe/', unsubscribe, name='unsubscribe')
 ]
