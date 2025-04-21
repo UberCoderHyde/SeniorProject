5e5ca2be3230657abad77bc8aaa9e780db97209a
Nikhita <nanusuri@charlotte.edu>
2025-04-21 08:46:08 -0400

    add email settings

--- recipe-suggester-backend/backend/settings.py
+++ recipe-suggester-backend/backend/settings.py
@@ -136,3 +136,13 @@ CSRF_COOKIE_HTTPONLY = True
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
+# Email settings
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "recipe.suggester.bot@gmail.com"
+EMAIL_HOST_PASSWORD = "digj vjuq wxsx aweb"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
